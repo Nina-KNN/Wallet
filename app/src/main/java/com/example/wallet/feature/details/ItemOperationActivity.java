0ce9444beb9333f2d0018f929d54daf70cc7719b
Nina <n.kruchinkina22022@gmail.com>
2020-07-15 23:35:54 +0300

    In ItemOperationActivity change CheckBox to Button "Profit" and "Expense". And add possibility to change profit and expense using screen swipe.

--- app/src/main/java/com/example/wallet/feature/details/ItemOperationActivity.java
+++ app/src/main/java/com/example/wallet/feature/details/ItemOperationActivity.java
@@ -2,6 +2,8 @@ package com.example.wallet.feature.details;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,10 +13,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,6 +29,7 @@ import com.example.wallet.data.icons.IconObject;
 import com.example.wallet.data.icons.IconsItemStoreProvider;
 import com.example.wallet.feature.details.base.BaseActivity;
 import com.example.wallet.feature.list.CalendarDialog;
+import com.example.wallet.feature.list.OnSwipeTouchListener;
 import com.example.wallet.feature.list.WorkWithDate;
 import com.example.wallet.feature.list.adapter.IconsListAdapter;
 
@@ -39,9 +41,14 @@ import static com.example.wallet.feature.details.BalanceListActivity.ITEMS_ID;
 import static com.example.wallet.feature.details.BalanceListActivity.PROFIT_VALUE;
 
 public class ItemOperationActivity extends BaseActivity implements View.OnClickListener{
+    RelativeLayout relativeLayout;
+
+    private TextView profitTextView;
+    private TextView expenseTextView;
+    private View profitLineView;
+    private View expenseLineView;
     private TextView dateTextView;
     private TextView titleTextView;
-    private CheckBox choiceProfitCheckBox;
     private ImageView imageImageView;
     private TextView iconNameTextView;
     private EditText sumEditText;
@@ -62,20 +69,28 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
 
     @Override
     protected void initView() {
-        dateTextView = findViewById(R.id.date_positive_operation);
-        titleTextView = findViewById(R.id.title_positive_operation);
-        choiceProfitCheckBox = findViewById(R.id.choice_profit_positive_operation);
-        iconNameTextView = findViewById(R.id.icon_name_operation);
-        sumEditText = findViewById(R.id.enter_positive_operation);
-        commentEditText = findViewById(R.id.enter_comment_positive_operation);
+        relativeLayout = findViewById(R.id.content_item_operation);
 
-        imageImageView = findViewById(R.id.image_value_operation);
+        profitTextView = findViewById(R.id.profit_item_operation);
+        expenseTextView = findViewById(R.id.expense_item_operation);
+        profitLineView = findViewById(R.id.profit_line_item_operation);
+        expenseLineView = findViewById(R.id.expense_line_item_operation);
+        dateTextView = findViewById(R.id.date_item_operation);
+        titleTextView = findViewById(R.id.title_item_operation);
+        iconNameTextView = findViewById(R.id.icon_name_item_operation);
+        sumEditText = findViewById(R.id.enter_item_operation);
+        commentEditText = findViewById(R.id.enter_comment_item_operation);
+
+        profitTextView.setOnClickListener(this);
+        expenseTextView.setOnClickListener(this);
+        imageImageView = findViewById(R.id.image_item_operation);
         imageImageView.setOnClickListener(this);
-        findViewById(R.id.save_button_positive_operation).setOnClickListener(this);
+        findViewById(R.id.save_button_item_operation).setOnClickListener(this);
         findViewById(R.id.button_back_item_operation).setOnClickListener(this);
         findViewById(R.id.calendar_image_button_item_operation).setOnClickListener(this);
 
         fillAndShowAllData();
+        makeTouchListener(this);
     }
 
     private void fillAndShowAllData() {
@@ -86,8 +101,8 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
             // Если объект уже существует в списке
             balance = BalanceItemStoreProvider.getInstance(this).getById(id);
             date.setTime(balance.getDate());
+            profit = balance.isChoiceProfit();
 
-            makeChoice(balance.isChoiceProfit());
             dateTextView.setText(WorkWithDate.showDateUtilsFormat(date, this));
             sumEditText.setText(String.valueOf(Math.abs(balance.getOperationSum())));
             commentEditText.setText(balance.getComment());
@@ -96,23 +111,27 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
             IconObject icon = IconsItemStoreProvider.getInstance(this).getIconById(categoryId);
             imageImageView.setImageResource(icon.getIconImage());
             iconNameTextView.setText(icon.getIconName());
+
+            titleTextView.setText(R.string.edit_note);
+
         } else {
             // Если новый объект
             profit = (boolean) intent.getSerializableExtra(PROFIT_VALUE);
             balance = new Balance();
-            makeChoice(profit);
             dateTextView.setText(WorkWithDate.showDateUtilsFormat(date, this));
+
+            titleTextView.setText(R.string.add_new_note);
         }
 
+        profitOrExpenseWasChoice(profit);
         enterOperationSum();
         enterComment();
-        makeCheckBoxListener();
     }
 
     private void makeRecyclerView(boolean profit) {
         List<IconObject> iconsList = IconsItemStoreProvider.getInstance(this).getIconsList(profit, true);
 
-        recyclerView = findViewById(R.id.recycler_positive_operation);
+        recyclerView = findViewById(R.id.recycler_item_operation);
         recyclerView.setLayoutManager(new GridLayoutManager(this, columnsCount()));
         recyclerView.setAdapter(new IconsListAdapter(iconsList, itemListener));
         recyclerView.setVisibility(View.VISIBLE);
@@ -153,6 +172,7 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
         } else {
             // Сохранить созданный элемент или обновить существующий
             sumEditText.setText(String.valueOf(Math.abs(balance.getOperationSum())));
+            balance.setChoiceProfit(profit);
             balance.setDate(date.getTime());
             WorkWithDate.checkCorrectDateInPrefsUtils(date.getTimeInMillis(), this);
 
@@ -212,30 +232,6 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
         });
     }
 
-    private void makeCheckBoxListener() {
-        choiceProfitCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                makeChoice(isChecked);
-                imageImageView.setImageResource(R.drawable.ic_touch_app);
-                iconNameTextView.setText(R.string.make_choice);
-                makeRecyclerView(profit);
-            }
-        });
-    }
-
-    private void makeChoice(boolean choice) {
-        profit = choice;
-        balance.setChoiceProfit(choice); // изменение поля в объекте
-        choiceProfitCheckBox.setChecked(choice);
-
-        if(choice) {
-            titleTextView.setText(R.string.title_profit);
-        } else {
-            titleTextView.setText(R.string.title_expense);
-        }
-    }
-
     // Скрыть клавиатуру при нажатии на пустое место на экране
     @Override
     public boolean dispatchTouchEvent(@NonNull MotionEvent event) {
@@ -252,14 +248,68 @@ public class ItemOperationActivity extends BaseActivity implements View.OnClickL
         return super.dispatchTouchEvent(event);
     }
 
+    // В зависимости от выбора "Доход"или "Расход" выделить выбранное
+    private void profitOrExpenseWasChoice(boolean isProfit) {
+        Resources res = getResources();
+        int lineColor = res.getColor(R.color.colorBlueDark);
+
+        if(isProfit) {
+            profitTextView.setTextAppearance(getApplicationContext(), R.style.boldText);
+            expenseTextView.setTextAppearance(getApplicationContext(), R.style.normalText);
+            profitLineView.setBackgroundColor(lineColor);
+            expenseLineView.setBackgroundColor(Color.TRANSPARENT);
+        } else {
+            profitTextView.setTextAppearance(getApplicationContext(), R.style.normalText);
+            expenseTextView.setTextAppearance(getApplicationContext(), R.style.boldText);
+            profitLineView.setBackgroundColor(Color.TRANSPARENT);
+            expenseLineView.setBackgroundColor(lineColor);
+        }
+    }
+
+    // Изменить значение "Доход"или "Расход" и показать список категорий
+    private void changeProfitValue(boolean isProfit) {
+        profit = isProfit;
+        profitOrExpenseWasChoice(profit);
+        imageImageView.setImageResource(R.drawable.ic_touch_app);
+        iconNameTextView.setText(R.string.make_choice);
+        makeRecyclerView(profit);
+    }
+
+    // Действия при свайпах в разные стороны
+    private void makeTouchListener(final Context currentActivity) {
+        relativeLayout.setOnTouchListener(new OnSwipeTouchListener(currentActivity) {
+            public void onSwipeRight() {
+                if(profit != true) {
+                    changeProfitValue(true);
+                }
+            }
+
+            public void onSwipeLeft() {
+                if(profit != false) {
+                    changeProfitValue(false);
+                }
+            }
+        });
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.image_value_operation:
+            case R.id.profit_item_operation:
+                if(profit != true) {
+                    changeProfitValue(true);
+                }
+                break;
+            case R.id.expense_item_operation:
+                if(profit != false) {
+                    changeProfitValue(false);
+                }
+                break;
+            case R.id.image_item_operation:
                 makeRecyclerView(profit);
                 break;
 
-            case R.id.save_button_positive_operation:
+            case R.id.save_button_item_operation:
                 saveBalanceData();
                 break;
 

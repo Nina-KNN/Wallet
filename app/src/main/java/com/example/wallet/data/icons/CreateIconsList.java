ff4e30dc9ea33c6b9e7e53bd3409f53be3b1fcd0
Nina <n.kruchinkina22022@gmail.com>
2020-09-08 19:57:31 +0300

    Change image for Category

--- app/src/main/java/com/example/wallet/data/icons/CreateIconsList.java
+++ app/src/main/java/com/example/wallet/data/icons/CreateIconsList.java
@@ -14,26 +14,32 @@ public class CreateIconsList {
         List<IconObject> iconsList = new ArrayList<>();
 
         iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_cocktail, "cocktail", false, true, 1));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_gas_station, "gas_station", false, true, 2));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_kye, "kye", false, true, 3));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_menu, "menu", false, true, 4));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_shopping_basket, "shopping_basket", false, true, 5));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_watch, "watch", false, true, 6));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_necklace, "necklace",false, true, 7));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_cat, "cat", false, true, 2));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_diploma, "diploma", false, true, 3));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_flower, "flower", false, true, 4));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_house, "house", false, true, 5));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_palm, "rest", false, true, 6));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_plane, "plane",false, true, 7));
         iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_cup, "cup", false, true, 8));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_bus, "bus", false, true, 9));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_car, "car", false, true, 10));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_present, "present", false, true, 9));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_shoes, "shoes", false, true, 10));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_toys, "toys", false, true, 11));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_travel_suitcase, "travel_suitcase", false, true, 12));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_tv, "tv", false, true, 13));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.ex_womens_boots, "womens_boots", false, true, 14));
 
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_coins_1, "coins_1", true, true, 11));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_calculator, "calculator", true, true, 12));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_cash_back, "cash_back", true, true, 13));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_hands_1, "hands_1", true, true, 14));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_safe, "safe", true, true, 15));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_coins_2, "coins_2", true, true, 16));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_coins_3, "coins_3", true, true, 17));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_growth_chart, "growth_chart", true, true, 18));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_men, "men", true, true, 19));
-        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_money, "money",true, true, 20));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_coins_1, "coins_1", true, true, 15));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_command_work_1, "command_work", true, true, 16));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_cash_back, "cash_back", true, true, 17));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_command_work_2, "command_work_2", true, true, 18));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_component, "save_money", true, true, 19));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_hand_with_money, "hand_with_money", true, true, 20));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_money_and_coins, "money_and_coins", true, true, 21));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_growth_chart, "growth_chart", true, true, 22));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_two_hand_with_money, "two_hand_with_money", true, true, 23));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_briefcase, "briefcase",true, true, 24));
+        iconsList.add(new IconObject(UUID.randomUUID(), R.drawable.p_wallet, "wallet",true, true, 25));
+
 
         return iconsList;
     }
